12525d8949479e68c81b196ae2d359f244d5d2f7
Oleg Schegol <olschegolev@yahoo.com>
2023-04-14 16:20:25 +0500

    Add project files.

---
+++ ASP_Restaurant/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using ASP_Restaurant.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_Restaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Booking()
+        {
+            return View();
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP_Restaurant/Models/BookTable.cs
@@ -0,0 +1,12 @@
+﻿namespace ASP_Restaurant.Models
+{
+    public class BookTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public string Review { get; set; }
+        public int NumberOfPersons { get; set; }
+    }
+}
